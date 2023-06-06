172ff38475984353bf5fa07ed10dd511fe7c6f19
Salvatore Corsaro <salvatore.corsaro@ironhack.com>
2023-06-07 02:32:50 +0900

    end maven part

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -1,7 +1,23 @@
 package com.ironhack;
 
+import com.google.gson.Gson;
+import com.ironhack.model.User;
+import net.datafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Faker faker = new Faker();
+        Gson gson = new Gson();
+
+
+        System.out.println("Hello " + faker.funnyName().name());
+        System.out.println(faker.yoda().quote());
+
+        User user = new User("salva", "salva@mail.com");
+        String userAsJson = gson.toJson(user);
+        System.out.println(userAsJson);
+        User user2 = gson.fromJson(userAsJson, User.class);
+        System.out.println(user2.getName());
     }
 }
